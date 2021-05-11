156f1db1f4b03cddfe53c3df50be1821786df08a
gurcangul <grcngl35@gmail.com>
2021-05-11 17:56:17 +0300

    Users added

---
+++ src/User/Buyer.java
@@ -0,0 +1,8 @@
+package User;
+
+public class Buyer implements User {
+	@Override
+    public void draw() {
+		System.out.println("Inside Buyer::draw() method.");
+	}
+}
---
+++ src/User/Seller.java
@@ -0,0 +1,8 @@
+package User;
+
+public class Seller implements User{
+	@Override
+    public void draw() {
+		System.out.println("Inside Seller::draw() method.");
+	}
+}
