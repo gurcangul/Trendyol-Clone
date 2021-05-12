39e629b89cd4fcdb199fb8a07ab3f22f967090cd
AYNUR ATIŞ <73234870+AynurAtis@users.noreply.github.com>
2021-05-12 19:25:50 +0300

    user package updated by A

--- src/User/UserFactory.java
+++ src/User/UserFactory.java
@@ -2,18 +2,19 @@ package User;
 
 public class UserFactory {
 	   //use getShape method to get object of type shape 
-	   public User getShape(String userType){
+	   public User getUser(int userID, String userType, String userName, String email, String password){
 	      if(userType == null){
 	         return null;
 	      }		
 	      if(userType.equalsIgnoreCase("Admin")){
-	         return new Admin();
+	    	  
+	         return new Admin(userID, userType, userName, email, password);
 	         
-	      } else if(userType.equalsIgnoreCase("Admin")){
-	         return new Admin();
+	      } else if(userType.equalsIgnoreCase("Buyer")){
+	         return new Buyer(userID, userType, userName, email, password);
 	         
 	      } else if(userType.equalsIgnoreCase("Seller")){
-	         return new Seller();
+	         return new Seller(userID, userType, userName, email, password);
 	      }
 	      
 	      return null;
