24ae5250877b13767a50c88ad94d668e269d82f2
gurcangul <grcngl35@gmail.com>
2021-05-12 19:29:36 +0300

    Factory has changed

--- src/App/OnlineShoppingManager.java
+++ src/App/OnlineShoppingManager.java
@@ -17,19 +17,19 @@ public class OnlineShoppingManager {
 
 		    System.out.println("....");
 		    //get an object of Admin and call its createObject method.
-		    User user1 = userFactory.getShape("Admin");
+		    User user1 = userFactory.getUser("Admin");
 		    
 		    //call createObject method of Admin
 		    user1.createObject();
 
 		    //get an object of Buyer and call its createObject method.
-		    User user2 = userFactory.getShape("Buyer");
+		    User user2 = userFactory.getUser("Buyer");
 
 		    //call createObject method of Buyer
 		    user2.createObject();
 
 		    //get an object of Square and call its createObject method.
-		    User user3 = userFactory.getShape("Seller");
+		    User user3 = userFactory.getUser("Seller");
 
 		    //call createObject method of Seller
 		    user3.createObject();
--- src/User/UserFactory.java
+++ src/User/UserFactory.java
@@ -2,15 +2,15 @@ package User;
 
 public class UserFactory {
 	   //use getShape method to get object of type shape 
-	   public User getShape(String userType){
+	   public User getUser(String userType){
 	      if(userType == null){
 	         return null;
 	      }		
 	      if(userType.equalsIgnoreCase("Admin")){
 	         return new Admin();
 	         
-	      } else if(userType.equalsIgnoreCase("Admin")){
-	         return new Admin();
+	      } else if(userType.equalsIgnoreCase("Buyer")){
+	         return new Buyer();
 	         
 	      } else if(userType.equalsIgnoreCase("Seller")){
 	         return new Seller();
