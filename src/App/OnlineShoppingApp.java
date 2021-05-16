cedffbc6ee61fae317e4ab814ad0e9cf19431da0
gurcangul <grcngl35@gmail.com>
2021-05-16 23:06:37 +0300

    DataHandler is fixed

--- src/App/OnlineShoppingApp.java
+++ src/App/OnlineShoppingApp.java
@@ -12,7 +12,7 @@ import Product.IProduct;
 public class OnlineShoppingApp {
 	/*
 	 * Aeayüzdeki Buttonlar sadece birer kere çalışıyor, tıklanıldıktan sonra bir daha çalışmıyor, ufak bir sorun fakat bulamadım. yarın tekrar deneyecegim. 
-	 *** */
+	 ** */
 	
 	public static void main(String args[]) throws FileNotFoundException, IOException, ParseException {
 		//DataHandler.getProductAndCategories();
